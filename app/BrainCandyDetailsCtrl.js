7a6c3f2447fb0adf6c643054ecb2cfbf6a267419
Jan Kowalski <jankowalski@vp.pl>
2016-05-25 12:23:27 +0200

    Exercise2

--- app/BrainCandyDetailsCtrl.js
+++ app/BrainCandyDetailsCtrl.js
@@ -1,13 +1,12 @@
 (function () {
-    'use strict';
+    "use strict";
     function BrainCandyDetailsCtrl(CandyDAO, $routeParams) {
         var ctrl = this;
         CandyDAO.get($routeParams.id).then(function (data) {
-              if(data !=0 ){
-                  ctrl.details = data;
-              }
+            if(data !==0 ){
+                ctrl.details = data;
+            }
         });
-
         ctrl.saveCandy = function () {
             CandyDAO.save(ctrl.details);
         };
@@ -15,7 +14,6 @@
             CandyDAO.delete($routeParams.id);
         };
     }
-
-    var module = angular.module('exerciseApp');
-    module.controller("BrainCandyDetailsCtrl", ['CandyDAO', '$routeParams', BrainCandyDetailsCtrl]);
+    var module = angular.module("exerciseApp");
+    module.controller("BrainCandyDetailsCtrl", ["CandyDAO", "$routeParams", BrainCandyDetailsCtrl]);
 })();
--- app/CandyDAO.js
+++ app/CandyDAO.js
@@ -1,17 +1,14 @@
 (function () {
-    'use strict';
-
+    "use strict";
     function CandyDAO($resource) {
         var api = $resource("/api/candy/:a", null, {
             query: {
                 isArray: false
             }
-        })
-        var sentence = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat';
-
+        });
         return {
             query: function () {
-                return api.query().$promise
+                return api.query().$promise;
             },
             get: function (id) {
                 return api.get({a: id}).$promise;
@@ -20,11 +17,9 @@
                 return api.save(object).$promise;
             },
             delete: function (id) {
-                var apis;
                 return api.delete({a: id}).$promise;
             }
         };
     }
-
-    angular.module('exerciseApp').factory('CandyDAO', ['$resource', CandyDAO]);
+    angular.module("exerciseApp").factory("CandyDAO", ["$resource", CandyDAO]);
 })();
